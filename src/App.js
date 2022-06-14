ae1ac81f7511e08d43af4a95e3be5ac0033cec5d
Shamiqureshi <Sssalman.sq@gmail.com>
2022-06-14 18:54:33 +0530

    Jsx review React.createElement/nested create ele

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import React,{ Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        React.createElement('div',{className:'App'},
+        React.createElement('h1', {}, 'My favorite ice cream flavors'),
+        React.createElement('ul', {},
+      [
+        React.createElement('li', {}, 'Chocolate'),
+        React.createElement('li', {}, 'Vanilla'),
+        React.createElement('li', {}, 'Banana')
+      ]
+        )
+))
+  }
 }
 
-export default App;
+export default App
