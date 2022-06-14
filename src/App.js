36fbb9710608ec553a49580af016b6def01c16ec
Shamiqureshi <Sssalman.sq@gmail.com>
2022-06-14 23:26:42 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React,{ Component } from 'react'
 import './App.css'
+import Navbar from './components/layout/Navbar'
 
 class App extends Component {
   render() {
-    const Name = 'John Doe';
-    const loading = true;
-    const Nobody = 'anyone is there!';
+    
     return (
         <div className='App'>
-          <h1>My App</h1>
-          {loading ? <h4>Loading...</h4>:<h4>{Name}</h4>}
+          <Navbar title="Github Finder" icon='fab fa-github'/>
         </div>
         )
   }
